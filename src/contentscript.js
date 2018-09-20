ad20a14cede79403d5ad4067b9ddf89d882c96ac
Kantouzin <kantouzin0113@gmail.com>
2018-09-20 15:33:32 +0900

    Add function of setting to open in window or tab

--- src/contentscript.js
+++ src/contentscript.js
@@ -1,9 +1,26 @@
 (function () {
+    var is_open_tab;
+
+    chrome.storage.local.get(
+        "is_open_tab",
+        (value) => {
+            if (value.is_open_tab == null) {
+                is_open_tab = false;
+            } else {
+                is_open_tab = value.is_open_tab;
+            }
+        }
+    )
+
     var observer = new MutationObserver(
         (mutationRecords, observer) => {
             mutationRecords.forEach((record) => {
                 if ("href" in record.target) {
-                    window.open(record.target.href, "_blank");
+                    if (is_open_tab) {
+                        window.open(record.target.href, "_blank");
+                    } else {
+                        document.location.href = record.target.href;
+                    }
                     observer.disconnect();
                 }
             });
@@ -16,6 +33,13 @@
         attributes: true,
         subtree: true
     };
+
+    chrome.runtime.onMessage.addListener(
+        (message, sender, sendResponse) => {
+            is_open_tab = message.value;
+            sendResponse();
+        }
+    );
 
     observer.observe(target, options);
 }());
